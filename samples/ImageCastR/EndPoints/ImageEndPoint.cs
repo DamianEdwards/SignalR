6fb92b4ce846e2c04ee64486b8ee6caddd3a1f02
Mikael Mengistu <mimengis@microsoft.com>
2017-05-09 14:35:31 -0700

    ImageCastR web project

---
+++ samples/ImageCastR/EndPoints/ImageEndPoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Sockets;
+
+namespace ImageCastR.EndPoints
+{
+    public class ImageEndPoint : EndPoint
+    {
+        public ConnectionList Connections { get; } = new ConnectionList();
+
+        public async override Task OnConnectedAsync(Connection connection)
+        {
+            Connections.Add(connection);
+
+            try
+            {
+                while (await connection.Transport.Input.WaitToReadAsync())
+                {
+                    Message message;
+                    if (connection.Transport.Input.TryRead(out message))
+                    {
+                        await Broadcast(message.Payload, message.Type, message.EndOfMessage);
+                    }
+                }
+            }
+            finally
+            {
+                Connections.Remove(connection);
+            }
+        }
+
+        private Task Broadcast(string text)
+        {
+            return Broadcast(Encoding.UTF8.GetBytes(text), MessageType.Text, endOfMessage: true);
+        }
+
+        private Task Broadcast(byte[] payload, MessageType format, bool endOfMessage)
+        {
+            var tasks = new List<Task>(Connections.Count);
+
+            foreach (var c in Connections)
+            {
+                tasks.Add(c.Transport.Output.WriteAsync(new Message(
+                    payload,
+                    format,
+                    endOfMessage)));
+            }
+            return Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ samples/ImageCastR/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ImageCastR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .ConfigureLogging((context, factory) =>
+                {
+                    factory.UseConfiguration(context.Configuration.GetSection("Logging"));
+                    factory.AddConsole();
+                    factory.AddDebug();
+                })
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
